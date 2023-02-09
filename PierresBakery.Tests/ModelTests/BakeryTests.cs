2ead794b4d97c61516567372da6bfaf03916193b
Tanya Prado <tanyaprado@gmail.com>
2023-02-09 09:15:43 -0800

    add initial project files

---
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+
+  }
+}
